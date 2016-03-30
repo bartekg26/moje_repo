ec3c5645948e46c3f81625a0095f4ad03dca3b6a
bartekg26 <bxaxrx12@wp.pl>
2016-03-30 17:04:52 +0200

    coooo

--- ConsoleApplication1 hello/Program.cs
+++ ConsoleApplication1 hello/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1_hello
         static void Main(string[] args)
         {
 
-            Console.WriteLine("oks77");
+            Console.WriteLine("oks7777");
 
         }
     }
