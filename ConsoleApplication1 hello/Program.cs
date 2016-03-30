59d0a0756ae88d6edd37073ce6c7db2835bc8ca5
bartekg26 <bxaxrx12@wp.pl>
2016-03-30 18:39:50 +0200

    yyyyyyyy

--- ConsoleApplication1 hello/Program.cs
+++ ConsoleApplication1 hello/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1_hello
         static void Main(string[] args)
         {
 
-            Console.WriteLine("oks7777");
+            Console.WriteLine("oks7777yyyy");
 
 
         }
