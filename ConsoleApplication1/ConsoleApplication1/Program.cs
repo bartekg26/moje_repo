8accf8a18128634a9b84774009cedd737ca2823e
Bartek <bxaxrx12@wp.pl>
2016-04-02 18:37:49 +0200

    ooooo

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("master");
-            Console.WriteLine("ok111yyyuuuuuuuu");
+            Console.WriteLine("ok111yyyuuuuuuu");
             Console.WriteLine("druga zmiana");
         }
     }
