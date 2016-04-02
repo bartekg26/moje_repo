0719fd74d4df81d441e31a7e6595fb023834ed7f
Bartek <bxaxrx12@wp.pl>
2016-04-02 18:07:42 +0200

    kolejny comit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ok");
+            Console.WriteLine("ok111");
         }
     }
 }
