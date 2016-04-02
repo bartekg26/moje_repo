07be82152e7e6d9a7436447177e78ec9a24e54a1
Bartek <bxaxrx12@wp.pl>
2016-04-02 18:26:14 +0200

    commit 1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("master");
             Console.WriteLine("ok111yyy");
             Console.WriteLine("druga zmiana");
         }
