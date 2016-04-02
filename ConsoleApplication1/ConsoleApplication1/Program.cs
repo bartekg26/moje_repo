7f81300897c100dca441fe10d04dd45e81c2e2b5
Bartek <bxaxrx12@wp.pl>
2016-04-02 18:19:23 +0200

    scalone

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ok111 2222");
+            Console.WriteLine("ok111");
             Console.WriteLine("druga zmiana");
         }
     }
